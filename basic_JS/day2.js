207c46b8e89f2ff3fef84b5e6132264bdcea39e9
Kaustubh Mani <153409797+ManiKaustubh022@users.noreply.github.com>
2025-12-09 05:03:15 +0000

    to check which value will be displayed or not

--- basic_JS/day2.js
+++ basic_JS/day2.js
@@ -1,7 +1,26 @@
-let a=123
-const b="abc"
-var c=78
-d=88
+// let a=123
+// const b="abc"
+// var c=78
+// d=88
+// //display all the value of above variable
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// //try to change the value of each and every variable and display the error msg if any
+// a=54
+// b="klj"
+// c=90
+// d=76
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+let a="abc"
+const b=""
+let c=null
+let d;
 
 console.log(a)
 console.log(b)
