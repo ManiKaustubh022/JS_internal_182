a1c78daa19ec9127cc812ade2157d68c4e4ee590
Kaustubh Mani <153409797+ManiKaustubh022@users.noreply.github.com>
2025-12-10 05:00:56 +0000

    using array functions

--- basic_JS/day3.js
+++ basic_JS/day3.js
@@ -1,5 +1,22 @@
-function fun(n){
-    return n*2
+// function fun(n){
+//     return n*2
+// }
+// let ch=fun(72)
+// console.log(ch)
+
+// let arr=[1,2,3,4,5,"Mani","Kaustubh",ch]
+// console.log(arr)
+
+let fruits=["apple","banana","grapes","orange","dragonfruit"]
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i])
 }
-let ch=fun(72)
-console.log(ch)
+fruits.push("papaya")
+fruits.pop()
+fruits.unshift("Kiwi")
+fruits.shift()
+console.log(fruits.includes("mango"))
+console.log(fruits.indexOf("banana"))
+
+
+
