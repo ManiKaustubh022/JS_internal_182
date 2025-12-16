1ee9a2b033a9515d908753696388a44fd4157119
Kaustubh Mani <153409797+ManiKaustubh022@users.noreply.github.com>
2025-12-16 06:56:31 +0000

    C updated

---
+++ basic_c/Day7.cs
@@ -0,0 +1,43 @@
+using System;
+// public delegate void delegate1();
+// class Day7{
+//     public static void method1(){
+//         Console.WriteLine("This is method 1");
+//     }
+//     public static void method2(){
+//         Console.WriteLine("This is method 2");
+//     }
+//     public static void Main(){
+//         delegate1 d1=method1;
+//         d1+=method2;
+//         d1();
+//     }
+// }
+
+// class Day71{
+//     public static void show(string msg)
+//     {
+//         Console.WriteLine(msg);
+//     }
+//     public static bool isEven(int x)
+//     {
+//         return x%2==0;
+//     }
+//     public static void Main()
+//     {
+//         func<int,int> f1 = (x) =>
+//         {
+//             return x*x;
+//         };
+//         Console.Write(f1(6));
+
+//         Action<string> a1=show;
+//         a1("UNITD");
+
+//         Predicate<int> p1=isEven;
+//         Console.WriteLine(p1(45));
+
+
+//     }
+    
+// }
